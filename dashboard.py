0cd26ed73c9c43e3b9bb93f990a2613a3327f61e
kaioryu07 <kaioh3107@gmail.com>
2026-05-04 17:19:02 -0300

    Projeto Completo

--- dashboard.py
+++ dashboard.py
@@ -20,46 +20,46 @@ def main():
     if filtro_pais != 'Todos':
         data_filtrada = data_filtrada[data_filtrada['País'] == filtro_pais]
 
-        gf_lucroporsegmento = px.bar(
-            data_filtrada.groupby('Segmento')['Lucro'].sum().reset_index(),
-            x = 'Segmento', y = 'Lucro',
-            title='Lucro por Segmento',
-            color='Segmento',
-            text_auto=True
-        ) 
-        gf_lucroporsegmento.update_layout(showlegend=False)
+    gf_lucroporsegmento = px.bar(
+    data_filtrada.groupby('Segmento')['Lucro'].sum().reset_index(),
+    x = 'Segmento', y = 'Lucro',
+    title='Lucro por Segmento',
+    color='Segmento',
+    text_auto=True
+    ) 
+    gf_lucroporsegmento.update_layout(showlegend=False)
 
-        gf_vendas_tempo = px.line(
-            data_filtrada.groupby('Segmento')['Lucro'].sum().reset_index(),
-            x='Data', y='Vendas Brutas',
-            title='Vendas ao Longo do Tempo',
-            markers=True
-        )
+    gf_vendas_tempo = px.line(
+    data_filtrada.groupby('Data')['Vendas Brutas'].sum().reset_index(),
+    x='Data', y='Vendas Brutas',
+    title='Vendas ao Longo do Tempo',
+    markers=True
+    )
 
-        gf_venda_produto = px.pie(
-            data_filtrada.groupby('Segmento')['Lucro'].sum().reset_index(),
-            values='Unidades Vendidas', names='Produto',
-            title='Distribuição de Produtos Vendidos',
-        )
+    gf_venda_produto = px.pie(
+    data_filtrada.groupby('Unidades Vendidas')['Produto'].sum().reset_index(),
+    values='Unidades Vendidas', names='Produto',
+    title='Distribuição de Produtos Vendidos',
+    )
 
-        custo_lucro_data = data_filtrada.groupby(['Segmento'])[['COGS', 'Lucro']].sum().reset_index().melt(
-            id_vars='Segmento', value_vars=['COGS', 'Lucro'])
-        custo_lucro_data['value_formatado'] = custo_lucro_data['value'].apply(lambda x: f'R$ {x:.2f}')
+    custo_lucro_data = data_filtrada.groupby(['Segmento'])[['COGS', 'Lucro']].sum().reset_index().melt(
+        id_vars='Segmento', value_vars=['COGS', 'Lucro'])
+    custo_lucro_data['value_formatado'] = custo_lucro_data['value'].apply(lambda x: f'R$ {x:.2f}')
 
-        gf_custo_lucro = px.bar(
-            custo_lucro_data,
-            x='Segmento', y='value',
-            title='Relação Entre Custo e Lucro',
-            color='variable',
-            barmode='group',
-            text='value_formatado'
-        )
+    gf_custo_lucro = px.bar(
+    custo_lucro_data,
+    x='Segmento', y='value',
+    title='Relação Entre Custo e Lucro',
+    color='variable',
+    barmode='group',
+    text='value_formatado'
+    )
 
-        col1, col2 = st.columns(2)
-        col3, col4 = st.columns(2)
-        col1.plotly_chart(gf_lucroporsegmento, use_container_width=True)
-        col2.plotly_chart(gf_vendas_tempo, use_container_width=True)
-        col3.plotly_chart(gf_venda_produto, use_container_width=True)
-        col4.plotly_chart(gf_custo_lucro, use_container_width=True)
+    col1, col2 = st.columns(2)
+    col3, col4 = st.columns(2)
+    col1.plotly_chart(gf_lucroporsegmento, use_container_width=True)
+    col2.plotly_chart(gf_vendas_tempo, use_container_width=True)
+    col3.plotly_chart(gf_venda_produto, use_container_width=True)
+    col4.plotly_chart(gf_custo_lucro, use_container_width=True)
 
 main()
